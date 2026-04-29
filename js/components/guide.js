664aa882c44704462366259ab49d4e50863697df
Sai Varshini Devalla <165990562+varshini1004@users.noreply.github.com>
2026-04-29 21:10:28 +0530

    guide.js

--- js/components/guide.js
+++ js/components/guide.js
@@ -1,5 +1,4 @@
 import { EMERGENCY_GUIDE } from '../data/guides.js';
-
 export function renderGuide(situation) {
   const steps = EMERGENCY_GUIDE[situation] || [];
   document.getElementById('guideTitle').textContent = situation;
@@ -9,5 +8,5 @@ export function renderGuide(situation) {
       <div class="snc">${i+1}</div>
       <div class="stx">${s}</div>
     </div>
-  `).join('');
+  `).join(''); 
 } 
